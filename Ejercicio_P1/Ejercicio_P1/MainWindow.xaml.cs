cc09b1b5e89bec582788e1a27ac1e8ae809dee04
Enriranjan <61691171+Enriranjan@users.noreply.github.com>
2020-10-22 16:45:23 +0200

    Ejercicio 1 v2
    Agregate delete button

--- Ejercicio_P1/Ejercicio_P1/MainWindow.xaml.cs
+++ Ejercicio_P1/Ejercicio_P1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace Ejercicio_P1
 {
@@ -20,9 +21,72 @@ namespace Ejercicio_P1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //ImageBrush img1, img2, img3;
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            //img1 = (ImageBrush) this.Background;
+            
+        }
+
+        private void BotonInvierte_Click(object sender, RoutedEventArgs e)
+        {
+            string textoDestino = CajaTexto.Text;
+            string textoOrigen = "";
+
+            foreach(char letra in textoDestino)
+            {
+                textoOrigen = letra + textoOrigen;
+            }
+
+            ResultadoConversion.Text = textoOrigen;
+        }
+
+        private void BotonMayusculas_Click(object sender, RoutedEventArgs e)
+        {
+            string textoOrigen = CajaTexto.Text.ToUpper();
+            
+            ResultadoConversion.Text = textoOrigen;
+        }
+
+        private void BotonBorra_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new Window();
+            win.Content = "El texto ha sido borrado satisfactoriamente";
+            win.Title = "Aviso";
+            win.Height = 100;
+            win.Width = 300;
+            win.MaxHeight = 100;
+            win.MaxWidth = 300;
+
+            win.Show();
+
+            CajaTexto.Text = "";
+        }
+
+        private void SiguienteImagen_Click(object sender, RoutedEventArgs e)
+        {
+            bool backgroundChange = false;
+         
+
+            /*while (!backgroundChange && indx < background.Count)
+            {
+                if (this.Background == background[indx])
+                {
+                    backgroundChange = true;
+                }
+
+                indx++;
+            }
+
+            if(indx == 3)
+            {
+                indx = 0;
+            }
+
+            this.Background = background[indx];*/
         }
     }
 }
